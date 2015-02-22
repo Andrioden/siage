c7f7bcd00e7c92fd9c380ec138a04fa274c82034
Bjørn Tore Gjerde <btgjerde@gmail.com>
2015-02-22 02:41:39 +0100

    Updated LeagueController to use Players factory to load league list
    Updated LeagueController to use Players factory to load league list

--- static/app/home/leaguecontroller.js
+++ static/app/home/leaguecontroller.js
@@ -1,44 +1,6 @@
 ﻿var siAgeApp = angular.module('SiAgeApp');
 
-siAgeApp.controller('LeagueController', ['$scope', '$http',
-function($scope, $http) {
-	$scope.players = [];
-	init($scope, $http);
-
-	/*function init() {
-		$scope.players = [{
-			name : 'Shrubber',
-			rating : 1520,
-			winpct : 66,
-			favciv : 'Britons'
-		}, {
-			name : 'Android',
-			rating : 1543,
-			winpct : 73,
-			favciv : 'French'
-		}, {
-			name : 'FreddyFearless',
-			rating : 1523,
-			winpct : 69,
-			favciv : "Huns"
-		}];*/
-
-		function init($scope, $http) {
-			$http.get('/api/players').success(function(data) {
-				$scope.players = angular.fromJson(data);
-			});
-	};
-
-	$scope.addPlayer = function() {
-
-		if ($scope.newplayer != null && $scope.newplayer != '') {
-			$scope.players.push({
-				name : $scope.newplayer,
-				rating : 1500
-			});
-		};
-
-		// Clear input fields after push
-		$scope.newplayer = "";
-	};
-}]); 
+siAgeApp.controller('LeagueController',
+    function ($scope, Players) {
+        $scope.players = Players.query();
+    });
--- static/app/home/leagueservice.js
+++ static/app/home/leagueservice.js
@@ -1,1 +1,10 @@
-﻿
+﻿var siAgeApp = angular.module('SiAgeApp');
+
+siAgeApp
+    .factory('Players', function ($resource) {
+        return $resource('/api/players/:playerId', {}, {
+            query: {method: 'GET', params: {playerId: 'playerId'}, isArray: true}
+        })
+    });
+
+
