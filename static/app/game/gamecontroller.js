108d26001c0272bea45e808a87195650649f0562
Bjørn Tore Gjerde <btgjerde@gmail.com>
2015-02-26 23:41:35 +0100

    Added game view and controller

---
+++ static/app/game/gamecontroller.js
@@ -0,0 +1,13 @@
+﻿var siAgeApp = angular.module('SiAgeApp');
+
+siAgeApp.controller('GameController', $routeParams,
+    function ($scope, Game) {
+        Game.get($routeParams.gameId,
+            function (data) {
+                $scope.game = data;
+            },
+            function (error) {
+                $scope.gameInfoError = "Unable to load player info!";
+            })
+    });
+
