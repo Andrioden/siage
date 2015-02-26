4a1a062915877609f6222864d5bf2c5a4280e559
André Skoglund Hansen <andre.ok@online.no>
2015-02-27 00:33:32 +0100

    Can return data for one player by url id + logging added

--- api/players.py
+++ api/players.py
@@ -2,6 +2,7 @@
 
 import webapp2
 import json
+import logging
 from models import Player
 
 class PlayersListHandler(webapp2.RequestHandler):
@@ -17,30 +18,32 @@ class PlayersListHandler(webapp2.RequestHandler):
 
     def post(self):
         # PROCESS REQUEST
-        data = json.loads(self.request.body)
-        Player(nick = data['nick']).put()
+        request_data = json.loads(self.request.body)
+        Player(nick = request_data['nick']).put()
         
         # RETURN RESPONSE
         self.response.headers['Content-Type'] = 'application/text'
         self.response.out.write("POST (Save) received with data: " + self.request.body)
 
 class PlayerHandler(webapp2.RequestHandler):
-    def get(self, playerId):
-        # BUILD DATA
-        print "Finding player"
-        player_data = Player.get_by_id() 
+    def get(self, player_id):
+        logging.info("Returning data for player_id: %s", player_id)
         
+        # BUILD DATA 
+        player = Player.get_by_id(int(player_id))
+        
+        # RETURN RESPONSE
         self.response.headers['Content-Type'] = 'application/json'
-        obj = [
-            {'nickname': 'Andrioden', 'rating': 1670, 'winrate': '55', 'bestcivilization': 'Huns'}
-        ]
-        self.response.out.write(json.dumps(obj))
+        if player:
+            self.response.out.write(json.dumps(player.get_data()))
+        else:
+            self.response.out.write(json.dumps({'data': player}))
 
     def put(self, playerId):
         self.response.headers['Content-Type'] = 'application/text'
         self.response.out.write("PUT (Update) received with data: " + self.request.body)
 
 app = webapp2.WSGIApplication([
-    webapp2.Route(r'/api/players/', handler=PlayersListHandler, name='players'),
-    webapp2.Route(r'/api/players/<playerId:(\d+)>', handler=PlayerHandler, name='player'),
+    (r'/api/players/', PlayersListHandler),
+    (r'/api/players/(\d+)', PlayerHandler),
 ], debug=True)
