26eabe437d0a18b19def8609371e4f52381fed65
André Skoglund Hansen <andre.ok@online.no>
2015-03-04 00:16:10 +0100

    Game models added, gamesettings in 1 api call

--- api/gamesettings.py
+++ api/gamesettings.py
@@ -1,61 +1,12 @@
 import webapp2
 import json
-import models
+from models import Game
 
 class GameSettingsHandler(webapp2.RequestHandler):
-    def get(self, settingname=None):
+    def get(self):
         self.response.headers['Content-Type'] = 'application/json'
-
-        if settingname == 'gametypes':
-            obj = [
-                {'id': '1', 'label': 'Game type 1'},
-                {'id': '2', 'label': 'Game type 2'},
-                {'id': '3', 'label': 'Game type 3'}
-            ]
-        elif settingname == 'mapstyles':
-            obj = [
-                {'id': '1', 'label': 'Map style 1'},
-                {'id': '2', 'label': 'Map style 2'},
-                {'id': '3', 'label': 'Map style 3'}
-            ]
-
-        elif settingname == 'civilizations':
-            obj = [
-                {'id': '1', 'label': 'Aztecs'},
-                {'id': '2', 'label': 'Britons'},
-                {'id': '3', 'label': 'Byzantines'},
-                {'id': '4', 'label': 'Celts'},
-                {'id': '5', 'label': 'Chinese'},
-                {'id': '6', 'label': 'Franks'},
-                {'id': '7', 'label': 'Goths'},
-                {'id': '8', 'label': 'Huns'},
-                {'id': '9', 'label': 'Incas'},
-                {'id': '10', 'label': 'Indians'},
-                {'id': '11', 'label': 'Italians'},
-                {'id': '12', 'label': 'Japanese'},
-                {'id': '13', 'label': 'Koreans'},
-                {'id': '14', 'label': 'Magyars'},
-                {'id': '15', 'label': 'Mayans'},
-                {'id': '16', 'label': 'Mongols'},
-                {'id': '17', 'label': 'Persians'},
-                {'id': '18', 'label': 'Saracens'},
-                {'id': '19', 'label': 'Slavs'},
-                {'id': '10', 'label': 'Spanish'},
-                {'id': '21', 'label': 'Teutons'},
-                {'id': '22', 'label': 'Turks'},
-                {'id': '23', 'label': 'Vikings'}
-            ]
-        else:
-            obj = [
-                {'id': '1', 'label': 'Test1'},
-                {'id': '2', 'label': 'Test2'},
-                {'id': '3', 'label': 'Test3'}
-            ]
-
-
-        self.response.out.write(json.dumps(obj))
-
+        self.response.out.write(json.dumps(Game._settings_data()))
 
 app = webapp2.WSGIApplication([
-    webapp2.Route(r'/api/gamesettings/<settingname>', handler=GameSettingsHandler, name='gamesetting')
+    (r'/api/gamesettings/', GameSettingsHandler)
 ], debug=True)
--- models.py
+++ models.py
@@ -1,7 +1,44 @@
 from google.appengine.ext import ndb
 
 class Player(ndb.Model):
-    nick = ndb.StringProperty()
+    nick = ndb.StringProperty(required=True)
     
     def get_data(self):
         return {'id': self.key.id(), 'nick': self.nick}
+
+class Game(ndb.Model):
+    date = ndb.DateProperty(required=True)
+    game_type = ndb.StringProperty(required=True, choices=['GameType 1', 'GameType 2'])
+    map_size = ndb.StringProperty(required=True, choices=['small', 'LOL', 'big'])
+    map_type = ndb.StringProperty(required=True, choices=['typex', 'typey'])
+    starting_age = ndb.StringProperty(required=True, choices=['Dark Age', 'Feudual Age', 'Castle Age', 'Imperial Age'])
+    resources = ndb.StringProperty(required=True, choices=['low'])
+    difficulty = ndb.StringProperty(required=True, choices=['easy'])
+    fixed_position = ndb.BooleanProperty(required=True)
+    reveal_map = ndb.BooleanProperty(required=True)
+    full_technology = ndb.BooleanProperty(required=True)
+    population = ndb.IntegerProperty(required=True, choices=[50,100,200])
+    duration_seconds = ndb.IntegerProperty(required=True)
+    trebuchet_allowed = ndb.BooleanProperty(required=True)
+    @classmethod
+    def _settings_data(cls):
+        return {
+            'game_types': list(cls.game_type._choices),
+            'map_size': list(cls.map_type._choices),
+            'map_type': list(cls.map_type._choices),
+            'starting_age': list(cls.starting_age._choices),
+            'resources': list(cls.resources._choices),
+            'difficulty': list(cls.difficulty._choices),
+            'population': list(cls.population._choices)
+        }
+    
+class PlayerResult(ndb.Model):
+    player = ndb.KeyProperty(required=True, kind=Player)
+    game = ndb.KeyProperty(required=True, kind=Game)
+    is_winner = ndb.BooleanProperty(default=False)
+    score = ndb.IntegerProperty(required=True)
+    civilization = ndb.StringProperty(required=True, choices=['Aztec', 'Franks'])
+    
+class PlayerResultStats(ndb.Model):
+    player_result = ndb.KeyProperty(required=True, kind=PlayerResult)
+    rating = ndb.IntegerProperty(required=True)
