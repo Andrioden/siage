41cce8d022dc7c1ec1f590016f6131153d9d6d53
Bjørn Tore Gjerde <btgjerde@gmail.com>
2015-03-11 02:32:07 +0100

    No longer handle date format in controller

--- static/app/game/gamecontroller.js
+++ static/app/game/gamecontroller.js
@@ -5,11 +5,10 @@ siAgeApp.controller('GameController',
         Game.get({game_id: $routeParams.gameId},
             function (data) {
                 $scope.game = data;
-                $scope.game.date = new Date(data.date).toLocaleDateString();
                 $scope.game.duration_minutes = data.duration_seconds/60;
             },
             function (error) {
                 $scope.gameInfoError = "Unable to load game info!";
             })
-    });
+        });
 
