12eef2dbd2a21d2769942a0bd823316b4348aa13
Bjørn Tore Gjerde <btgjerde@gmail.com>
2015-04-06 20:47:20 +0200

    Login functionality part 2

--- api/users.py
+++ api/users.py
@@ -10,6 +10,13 @@ class UserHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
+            user_data = { 'user_name': user.nickname(), 'logged_in': True,  'player': Player.query(Player.userid == user.user_id()).get()};            
+        else:
+            return error_400(self.response, "VALIDATION_ERROR_NOT_LOGGED_INN", "The browsing user is not logged inn.")
+
+        # RETURN RESPONSE
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(json.dumps(user_data))
 
 class LoginHandler(webapp2.RequestHandler):
     def get(self):
@@ -25,4 +32,3 @@ app = webapp2.WSGIApplication([
     (r'/api/users/login', LoginHandler),
     (r'/api/users/logout', LogoutHandler)
 ], debug=True)
-
