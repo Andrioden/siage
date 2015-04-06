9d8de334e4042989fc6b8e884cce8819a866ee7e
Bjørn Tore Gjerde <btgjerde@gmail.com>
2015-04-06 22:14:49 +0200

    Only return nick, not all player data in user get request

--- api/users.py
+++ api/users.py
@@ -9,8 +9,9 @@ from utils import error_400
 class UserHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
+        existing_player = Player.query(Player.userid == user.user_id()).get()
         if user:
-            user_data = { 'user_name': user.nickname(), 'logged_in': True,  'player': Player.query(Player.userid == user.user_id()).get()};            
+            user_data = { 'user_name': user.nickname(), 'logged_in': True,  'player': existing_player.nick};            
         else:
             return error_400(self.response, "VALIDATION_ERROR_NOT_LOGGED_INN", "The browsing user is not logged inn.")
 
